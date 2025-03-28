0da2bb5d4ab63ff80eb4c4be01dacd4ad1085412
KEN23360 <emmuchena@kcbgroup.com>
2025-03-28 12:35:43 +0200

    Rename Controller class

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class DemoController {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoController.class, args);
     }
 
     @GetMapping("/hello")
